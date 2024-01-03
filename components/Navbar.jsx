06b0d0fe4c1528b3fb43c9d5f56ec1150422321e
Percobain <118096592+Percobain@users.noreply.github.com>
2024-01-03 19:42:48 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import "../src/App.css";
 export default function Navbar() {
     return (
         <nav>
-            <img src="./imgs/React-icon.svg.png" className="nav--icon" />
+            <img src="./imgs/React-icon.png" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
