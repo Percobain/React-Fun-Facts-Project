68e8610e9a370bb90a2ff64a087daf9aa371f5f3
PassengerCounterApp <shreyans.tatiya@gmail.com>
2024-01-03 19:28:40 +0530

    Initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "../src/App.css";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="./imgs/React-icon.svg.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
+
